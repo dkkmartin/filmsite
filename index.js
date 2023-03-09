e4d7a645d879a5ee0446b528ae0fb20b63024cfc
Martin <martinbruun1995@gmail.com>
2023-03-09 17:59:48 +0100

    Add: Animations
    Added random animation for each modal open and close

--- index.js
+++ index.js
@@ -12,17 +12,47 @@ function delay(time) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
+function randomAnimation() {
+  const randomInt = Math.floor(Math.random() * 4) + 1
+  if (randomInt === 1) {
+    const animationIn = 'animate__fadeInDown'
+    const animationOut = 'animate__fadeOutDown'
+    return {animationIn, animationOut}
+  }
+  if (randomInt === 2) {
+    const animationIn = 'animate__fadeInRight'
+    const animationOut = 'animate__fadeOutLeft'
+    return {animationIn, animationOut}
+  }
+  if (randomInt === 3) {
+    const animationIn = 'animate__fadeInLeft'
+    const animationOut = 'animate__fadeOutRight'
+    return {animationIn, animationOut}
+  }
+  if (randomInt === 4) {
+    const animationIn = 'animate__fadeInUp'
+    const animationOut = 'animate__fadeOutUp'
+    return {animationIn, animationOut}
+  }
+}
+
 menuBtn.addEventListener('click', () => {
+  const animation = randomAnimation()
   modalMenu.style.display = 'flex'
   footer.style.display = 'none'
+  modalMenu.classList.add(animation.animationIn)
+  delay(1000).then(() => {
+    modalMenu.classList.remove(animation.animationIn)
+  })
+  
 })
 
 modalClose.addEventListener('click', () => {
-  modalMenu.classList.add('animate__animated')
-  modalMenu.classList.add('animate__fadeOutDown')
+  const animation = randomAnimation()
+  modalMenu.classList.add(animation.animationOut)
   delay(1000).then(() => {
     modalMenu.style.display = 'none'
-    modalMenu.classList.remove('animate__fadeOutDown')
+    modalMenu.classList.remove(animation.animationOut)
   })
 })
 
